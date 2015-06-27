630a06645807616a5362f18b1a708c54f7dd601d
Pavel Diatchenko <diatche@gmail.com>
2015-06-27 20:26:51 +1000

    Initial Commit

---
+++ ChartsBootstrap/ChartsBootstrap.h
@@ -0,0 +1,19 @@
+//
+//  ChartsBootstrap.h
+//  ChartsBootstrap
+//
+//  Created by Pavel Diatchenko on 27/06/2015.
+//  Copyright (c) 2015 Pavel Diatchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ChartsBootstrap.
+FOUNDATION_EXPORT double ChartsBootstrapVersionNumber;
+
+//! Project version string for ChartsBootstrap.
+FOUNDATION_EXPORT const unsigned char ChartsBootstrapVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChartsBootstrap/PublicHeader.h>
+
+
